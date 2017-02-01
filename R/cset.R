27323210a3fa4fa297a9b86d5395ffc17257f3ed
PhilipPallmann <p.pallmann@lancaster.ac.uk>
2017-02-01 17:11:02 +0000

    S3 stuff

---
+++ R/cset.R
@@ -0,0 +1,327 @@
+cset <- function(dat, method, alpha=0.1, steps=100, TsengBrownA=1, TsengBrownB=1){
+  
+  n <- nrow(dat)
+  p <- ncol(dat)
+  df <- n - 1
+  est <- matrix(colMeans(dat), p)
+  poolvar <- var(as.vector(as.matrix(dat)))
+  cov <- cov(dat)
+  solved <- solve(cov)
+  #s2 <- poolvar/n
+  #s <- sqrt(poolvar/n)
+  
+  method <- match.arg(method, choices=c("bootkern", "emp.bayes", "hotelling", "limacon.asy", "limacon.fin",
+                                        "standard.cor", "standard.ind", "tseng", "tseng.brown"))
+  
+  if(method=="bootkern"){
+    
+    stop("Not implemented (yet).")
+    
+  }
+  
+  if(method=="emp.bayes"){
+    
+    ciFinal <- cbind(est - 2 * poolvar, est + 2 * poolvar)
+    
+    searchwidth <- 1
+    
+    while(min(abs(ciFinal[, 1] - (est - 2 * poolvar))) < 0.001 | min(abs(ciFinal[, 2] - (est + 2 * poolvar))) < 0.001){
+      
+      togrid <- list()
+      
+      for(i in 1:p){
+        togrid[[i]] <- seq(est[i] - searchwidth * poolvar, est[i] + searchwidth * poolvar, length.out=steps)
+      }
+      
+      grid <- expand.grid(togrid)
+      
+      a <- (p - 2) * (df / (df + 2))
+      JSfactor <- (1 - (a * (poolvar/n)) / sum(est^2)) # or just poolvar???
+      
+      if(JSfactor < 0){
+        JSplus <- est
+      }else{
+        JSplus <- JSfactor * est
+      }
+      
+      cond <- (sum(est^2) / (poolvar/n)) < (p * qf(p=1 - alpha, df1=p, df2=df)) # or just poolvar???
+      
+      if(cond==TRUE){
+        vE2 <- (1 - a/(p * qf(p=1 - alpha, df1=p, df2=df))) *
+          (p * qf(p=1 - alpha, df1=p, df2=df) - p * log(1 - a/(p * qf(p=1 - alpha, df1=p, df2=df))))
+      }else{
+        vE2 <- (1 - a/(p * qf(p=1 - alpha, df1=p, df2=df))) *
+          (p * qf(p=1 - alpha, df1=p, df2=df) - p * log(1 - a/(p * qf(p=1 - alpha, df1=p, df2=df))))
+      }
+      
+      findcr <- apply(grid, 1, function(x){
+        theta <- matrix(x, p)
+        sqrt(sum((theta - JSplus)^2)) < (sqrt(poolvar/n) * sqrt(vE2))
+      })
+      
+      crFinal <- cbind(grid, findcr)[findcr==1, ]
+      ciFinal <- t(apply(crFinal[, -(p + 1)], 2, range, na.rm=TRUE))
+      
+      searchwidth <- 2 * searchwidth
+      
+    }
+    
+  }
+  
+  if(method=="hotelling"){
+    
+    ciFinal <- cbind(est - 2 * poolvar, est + 2 * poolvar)
+    
+    searchwidth <- 1
+    
+    while(min(abs(ciFinal[, 1] - (est - 2 * poolvar))) < 0.001 | min(abs(ciFinal[, 2] - (est + 2 * poolvar))) < 0.001){
+      
+      togrid <- list()
+      
+      for(i in 1:p){
+        togrid[[i]] <- seq(est[i] - searchwidth * poolvar, est[i] + searchwidth * poolvar, length.out=steps)
+      }
+      
+      grid <- expand.grid(togrid)
+      
+      rhs <- qf(p=1 - alpha, df1=p, df2=df - p + 1) * p * df / (df - p + 1)
+      
+      findcr <- apply(grid, 1, function(x){
+        theta <- matrix(x, p)
+        (n * t(est - theta) %*% solved %*% (est - theta)) < rhs 
+      })
+      
+      crFinal <- cbind(grid, findcr)[findcr==1, ]
+      ciFinal <- t(apply(crFinal[, -(p + 1)], 2, range, na.rm=TRUE))
+      
+      searchwidth <- 2 * searchwidth
+      
+    }
+    
+  }
+  
+  if(method=="limacon.asy"){
+    
+    ciFinal <- cbind(est - 2 * poolvar, est + 2 * poolvar)
+    
+    searchwidth <- 1
+    
+    while(min(abs(ciFinal[, 1] - (est - 2 * poolvar))) < 0.001 | min(abs(ciFinal[, 2] - (est + 2 * poolvar))) < 0.001){
+      
+      togrid <- list()
+      
+      for(i in 1:p){
+        togrid[[i]] <- seq(est[i] - searchwidth * poolvar, est[i] + searchwidth * poolvar, length.out=steps)
+      }
+      
+      grid <- expand.grid(togrid)
+      
+      findcr <- apply(grid, 1, function(x){
+        theta <- matrix(x, p)
+        ((t(theta) %*% solved %*% est) / sqrt(t(theta) %*% solved %*% theta) * sqrt(n) + qnorm(1 - alpha)) >
+          (sqrt(t(theta) %*% solved %*% theta) * sqrt(n)) 
+      })
+      
+      crFinal <- cbind(grid, findcr)[findcr==1, ]
+      ciFinal <- t(apply(crFinal[, -(p + 1)], 2, range, na.rm=TRUE))
+      
+      searchwidth <- 2 * searchwidth
+      
+    }
+    
+  }
+  
+  if(method=="limacon.fin"){
+    
+    ciFinal <- cbind(est - 2 * poolvar, est + 2 * poolvar)
+    
+    searchwidth <- 1
+    
+    while(min(abs(ciFinal[, 1] - (est - 2 * poolvar))) < 0.001 | min(abs(ciFinal[, 2] - (est + 2 * poolvar))) < 0.001){
+      
+      togrid <- list()
+      
+      for(i in 1:p){
+        togrid[[i]] <- seq(est[i] - searchwidth * poolvar, est[i] + searchwidth * poolvar, length.out=steps)
+      }
+      
+      grid <- expand.grid(togrid)
+      
+      findcr <- apply(grid, 1, function(x){
+        theta <- matrix(x, p)
+        ((t(theta) %*% est) / sqrt((t(theta) %*% cov %*% theta) / n) + qt(1 - alpha, df)) >
+          ((t(theta) %*% theta) / sqrt((t(theta) %*% cov %*% theta) / n))
+      })
+      
+      crFinal <- cbind(grid, findcr)[findcr==1, ]
+      ciFinal <- t(apply(crFinal[, -(p + 1)], 2, range, na.rm=TRUE))
+      
+      searchwidth <- 2 * searchwidth
+      
+    }
+    
+  }
+  
+  if(method=="standard.cor"){
+    
+    ciFinal <- cbind(est - 2 * poolvar, est + 2 * poolvar)
+    
+    searchwidth <- 1
+    
+    while(min(abs(ciFinal[, 1] - (est - 2 * poolvar))) < 0.001 | min(abs(ciFinal[, 2] - (est + 2 * poolvar))) < 0.001){
+      
+      togrid <- list()
+      
+      for(i in 1:p){
+        togrid[[i]] <- seq(est[i] - searchwidth * poolvar, est[i] + searchwidth * poolvar, length.out=steps)
+      }
+      
+      grid <- expand.grid(togrid)
+      
+      findcr <- apply(grid, 1, function(x){
+        theta <- x
+        t(est - theta) %*% solved %*% (est - theta) < (p / (n - 1) * qf(p=1 - alpha, df1=p, df2=df))
+      })
+      
+      crFinal <- cbind(grid, findcr)[findcr==1, ]
+      ciFinal <- t(apply(crFinal[, -(p + 1)], 2, range, na.rm=TRUE))
+      
+      searchwidth <- 2 * searchwidth
+      
+    }
+    
+  }
+  
+  if(method=="standard.ind"){
+    
+    ciFinal <- cbind(est - 2 * poolvar, est + 2 * poolvar)
+    
+    searchwidth <- 1
+    
+    while(min(abs(ciFinal[, 1] - (est - 2 * poolvar))) < 0.001 | min(abs(ciFinal[, 2] - (est + 2 * poolvar))) < 0.001){
+      
+      togrid <- list()
+      
+      for(i in 1:p){
+        togrid[[i]] <- seq(est[i] - searchwidth * poolvar, est[i] + searchwidth * poolvar, length.out=steps)
+      }
+      
+      grid <- expand.grid(togrid)
+      
+      findcr <- apply(grid, 1, function(x){
+        theta <- x
+        sqrt(sum((est - theta)^2))^2 < (poolvar/n * p * qf(p=1 - alpha, df1=p, df2=df))
+      })
+      
+      crFinal <- cbind(grid, findcr)[findcr==1, ]
+      ciFinal <- t(apply(crFinal[, -(p + 1)], 2, range, na.rm=TRUE))
+      
+      searchwidth <- 2 * searchwidth
+      
+    }
+    
+  }
+  
+  if(method=="tseng"){
+    
+    ciFinal <- cbind(est - 2 * poolvar, est + 2 * poolvar)
+    
+    searchwidth <- 1
+    
+    while(min(abs(ciFinal[, 1] - (est - 2 * poolvar))) < 0.001 | min(abs(ciFinal[, 2] - (est + 2 * poolvar))) < 0.001){
+      
+      togrid <- list()
+      
+      for(i in 1:p){
+        togrid[[i]] <- seq(est[i] - searchwidth * poolvar, est[i] + searchwidth * poolvar, length.out=steps)
+      }
+      
+      grid <- expand.grid(togrid)
+      
+      lhs <- sqrt(sum(est^2))^2 / (p * poolvar/n)
+      
+      findcr <- apply(grid, 1, function(x){
+        theta <- x
+        lhs > qf(p=alpha, df1=p, df2=df, ncp=(sqrt(sum(theta^2))^2 / (poolvar/n)))
+      })
+      
+      crFinal <- cbind(grid, findcr)[findcr==1, ]
+      ciFinal <- t(apply(crFinal[, -(p + 1)], 2, range, na.rm=TRUE))
+      
+      searchwidth <- 2 * searchwidth
+      
+      if(nrow(crFinal)==0){
+        ciFinal <- matrix(rep(Inf, 2 * p), 2)
+        break
+      }
+      
+      if(ciFinal[1, 1]==Inf | ciFinal[1, 1]==-Inf){
+        ciFinal <- matrix(rep(0, 2 * p), 2)
+        break
+      }
+      
+    }
+    
+  }
+  
+  if(method=="tseng.brown"){
+    
+    ciFinal <- cbind(est - 2 * poolvar, est + 2 * poolvar)
+    
+    searchwidth <- 1
+    
+    while(min(abs(ciFinal[, 1] - (est - 2 * poolvar))) < 0.001 | min(abs(ciFinal[, 2] - (est + 2 * poolvar))) < 0.001){
+      
+      togrid <- list()
+      
+      for(i in 1:p){
+        togrid[[i]] <- seq(est[i] - searchwidth * poolvar, est[i] + searchwidth * poolvar, length.out=steps)
+      }
+      
+      grid <- expand.grid(togrid)
+      
+      findcr <- apply(grid, 1, function(x){
+        theta <- x
+        (sqrt(sum((est - theta * (1 + (1/(TsengBrownA + TsengBrownB * (sqrt(sum(theta^2))^2)))))^2))^2) <
+          qchisq(p=alpha, df=2, ncp=((sqrt(sum(theta^2))^2) * (1/(TsengBrownA + TsengBrownB * (sqrt(sum(theta^2))^2)))^2))
+      })
+      
+      crFinal <- cbind(grid, findcr)[findcr==1, ]
+      ciFinal <- t(apply(crFinal[, -(p + 1)], 2, range, na.rm=TRUE))
+      
+      searchwidth <- 2 * searchwidth
+            
+      if(nrow(crFinal)==0){
+        ciFinal <- matrix(rep(Inf, 2 * p), 2)
+        break
+      }
+      
+      if(ciFinal[1, 1]==Inf | ciFinal[1, 1]==-Inf){
+        ciFinal <- matrix(rep(0, 2 * p), 2)
+        break
+      }
+      
+    }
+    
+  }
+  
+  Out <- list
+  
+  Out$cr <- crFinal
+  Out$ci <- ciFinal
+  Out$n <- n
+  Out$p <- p
+  Out$est <- est
+  Out$cov <- cov
+  Out$poolvar <- poolvar
+  Out$df <- df
+  Out$dat <- dat
+  Out$method <- method
+  Out$alpha <- alpha
+  Out$steps <- steps
+  
+  class(Out) <- "JOC"
+  
+  return(Out)
+  
+}
---
+++ R/helpers.R
@@ -0,0 +1,41 @@
+print.JOC <- summary.JOC <- function(x, digits=max(3, getOption("digits") - 4), ...){
+  
+  cat(paste("Parameter estimates and projected boundaries of the ", x$p, "-dimensional ", 100 * (1 - x$alpha),
+            "% simultaneous confidence region\n", sep=""))
+  
+  res <- cbind(round(x$est, digits), round(x$ci, digits))
+  row.names(res) <- colnames(x$dat)
+  col.names(res) <- c("Estimate", "Lower", "Upper")
+  
+  print(res)
+}
+
+plot.JOC <- function(x, equi=log(c(0.8, 1.25)), axnames=NULL, main="Title", xlim=log(c(0.77, 1.3)),
+                     ylim=log(c(0.77, 1.3)), col="black", ...){
+  
+  if(is.null(axnames)==TRUE){
+    axisnames <- colnames(dat)
+  }else{
+    axisnames <- axnames
+  }
+  
+  par(mar=c(5, 5, 4, 2))
+  plot(0, xlim=xlim, ylim=ylim, las=1, xlab=axisnames[1], ylab=axisnames[2],
+       cex.main=2.5, cex.axis=1.5, cex.lab=1.7, main=main, ...)
+  if(is.null(equi)==FALSE){
+    if(length(equi)!=2){
+      stop("Length of equi must be 2.")
+    }
+    rect(equi[1], equi[1], equi[2], equi[2], col="gray95", border=NA)
+  }
+  if(method %in% c("limacon.asy", "limacon.fin", "tseng", "tseng.brown")){
+    points(crFinal[, -3], pch=20, col=col)
+  }
+  if(method %in% c("emp.bayes", "hotelling", "standard.cor", "standard.ind")){
+    polygon(crFinal[chull(crFinal[, -3]), -3], col=col, border=col)
+  }
+  points(est[1], est[2], pch=19, col="white")
+  points(0, 0, pch="+", col="white", cex=2)
+  par(mar=c(5, 4, 4, 2))
+  
+}
